b1c3bc89838e72a498ab429119121188b75ee497
christianq <reddjedet@protonmail.com>
2025-07-29 23:51:32 -0300

    finalizado el segundo ejercicio - agregar cada amigo como <li> al DOM

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 
+//- - - - - - - - - - - - - - - - - - - TAREA 1 - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //Crea un array para almacenar los nombres
 let listadoNombres = [];
 
 
-//Implementa una función para agregar amigos 
+//Implementa una función para agregar amigos al array
 function agregarAmigo() {
-    let input = document.querySelector("#amigo"); //Toma lo ingresado por input (id=amigo) y lo asocia a input
-    let nombre = input.value; //Nombre será dicho texto. Podría ser redundante tener 2 variables para el input
+    const input = document.querySelector("#amigo"); //Toma lo ingresado por input (id=amigo) y lo asocia a input
+    const nombre = input.value; //Nombre será dicho texto. Podría ser redundante tener 2 variables para el input
     console.log("Ingresó un nombre nuevo: " + nombre);
 
     if (nombre === "") {
@@ -18,7 +19,31 @@ function agregarAmigo() {
     }
 
     listadoNombres.push(nombre); //Agrega el nombre al array
-    console.log(`listadoNombres = ${listadoNombres}`) //Revisión
+    console.log(`listadoNombres = ${listadoNombres}`); //Revisión
     input.value = ""; //Limpiar el input antes de terminar la función
+
+    mantenerListadoAmigos();
 }
+
+
+
+/*
+    //Tarea 2 - Fué mi primer intento
+    let agregarLi = document.querySelector('#listaAmigos'); //toma el nuevo integrante del array
+    agregarLi.innerHTML += `<li>${nombre}</li>` //Y lo agrega al DOM en formato lista
+}
+*/
+
+
+function mantenerListadoAmigos() { 
+    const lista = document.querySelector('#listaAmigos'); 
+    lista.innerHTML = ""; //Crucial para evitar duplicaciones
+
+
+    for (let i = 0; i < listadoNombres.length; i++) { 
+        lista.innerHTML += `<li>${listadoNombres[i]}</li>`;
+    }
+}
+
+
 
